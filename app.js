a94de67238590ce3dfebbc17fbfd8bd928cecf5c
Mayank <ffmayank097@gmail.com>
2024-10-24 20:45:24 +0530

    Day-26 Movie App using API

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let award = document.querySelector('.award');
 let rating = document.querySelector('.rating');
 let rating2 = document.querySelector('.rating2');
 let rating3 = document.querySelector('.rating3');
+let rating4 = document.querySelector('.rating4');
 let para = document.querySelector('.para')
 let suggestions = document.querySelector('#suggestion');
 let boxoffice = document.querySelector('.boxoffice');
@@ -39,10 +40,15 @@ function send() {
             div.classList.add('hidden');
         }
 
+
+
+
         else{
+
         para.classList.remove('d');
         para.classList.add('hidden')
         document.getElementById('title').innerText = data.Title;
+        img.src = data.Poster;
         genre.innerText=data.Genre;
         released.innerText=data.Released;
         rated.innerText=data.Rated;
@@ -53,18 +59,68 @@ function send() {
         country.innerText=data.Country;
         award.innerText=data.Awards;
 
-        let s1 = data.Ratings[0].Source;
-        let s2 = data.Ratings[1].Source;
-        let s3 = data.Ratings[2].Source;
-        let v1 = data.Ratings[0].Value;
-        let v2= data.Ratings[1].Value;
-        let v3 = data.Ratings[2].Value;
 
-        rating.innerText=` ${s1} : ${v1} `;
-        rating2.innerText=`${s2} : ${v2}`;
-        rating3.innerText=`${s3} : ${v3}`
+        if(data.Ratings.length === 1){
+            let s1 = data.Ratings[0].Source;
+            let v1 = data.Ratings[0].Value;
+            rating.innerText=` ${s1} : ${v1} `;
+
+        }
+
+        else if (data.Ratings.length === 2){
+            let s1 = data.Ratings[0].Source;
+            let v1 = data.Ratings[0].Value;
+            rating.innerText=` ${s1} : ${v1} `;
+            let s2 = data.Ratings[1].Source;
+            let v2= data.Ratings[1].Value;
+            rating2.innerText=`${s2} : ${v2}`;
+
+        }
+
+        else if(data.Ratings.length === 3){
+            let s1 = data.Ratings[0].Source;
+            let v1 = data.Ratings[0].Value;
+
+            rating2.innerText=` ${s1} : ${v1} `;
+            let s2 = data.Ratings[1].Source;
+            let v2= data.Ratings[1].Value;
+            rating2.innerText=`${s2} : ${v2}`;
+
+
+            let s3 = data.Ratings[2].Source;
+            let v3 = data.Ratings[2].Value;
+            rating3.innerText=`${s3} : ${v3}`
+
+        }
+
+        else if (data.Ratings.length === 4){
+            let s1 = data.Ratings[0].Source;
+            let v1 = data.Ratings[0].Value;
+
+            rating2.innerText=` ${s1} : ${v1} `;
+            let s2 = data.Ratings[1].Source;
+            let v2= data.Ratings[1].Value;
+            rating2.innerText=`${s2} : ${v2}`;
+
+
+            let s3 = data.Ratings[2].Source;
+            let v3 = data.Ratings[2].Value;
+            rating3.innerText=`${s3} : ${v3}`
+
+
+            let s4 = data.Ratings[3].Source;
+            let v4 = data.Ratings[3].Value;
+            rating4.innerText=`${s4} : ${v4}`;
+
+        }
+
+
+
+
         boxoffice.innerText=data.BoxOffice;
-        img.src = data.Poster;
+
+
+
         div.classList.remove('hidden');
         div.classList.add('container');
 
